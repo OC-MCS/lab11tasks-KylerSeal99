9b4e8ed5230e8e9fba375f650cf896b93943b066
Kyler Seal <kyler.seal@eagles.oc.edu>
2019-04-01 17:27:51 -0500

    through task 3 *thumbs up*

--- NumberList/main.cpp
+++ NumberList/main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "NumberList.h"
 using namespace std;
 
 int main()
 {
-	 
+	NumberList *list;
+	list = new NumberList;
+	/*list->insertNode(10);
+	list->appendNode(40);
+	list->insertNode(30);
+	list->insertNode(25);
+	list->deleteNode(30);
+	list->displayList();
+	list->insertNode(20);
+*/
+	string tempString;
+	int tempInt;
+	fstream myFile;
+	myFile.open("10Ints.txt");
+	if (!myFile)
+	{
+		cout << "File not found \n";
+	}
+	else
+	{
+		while (getline(myFile, tempString))
+		{
+			list->insertNode(stoi(tempString));
+		}
+
+		list->displayList();
+	}
+
+
+
 	return 0;
 }
